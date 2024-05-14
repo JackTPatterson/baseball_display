97ca9ab4db48b3cef80c881a451af2f4d939e7cb
jackpatterson <jtpatt03@gmail.com>
2024-05-14 02:21:34 -0400

    ui done kinda

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,204 @@
-import Image from 'next/image'
+import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 
 export default function Home() {
+
+
+    async function getStats() {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", "Basic TkRFeE5qQTFNVFY4TVRjeE16azROelUzTlM0Mk1ESXpOemMwOjVkYTMwZWM3NTU0MjdiZjhmNGM4ZTM2ZTU0NTA2ZTg5Njg5ODhkMmIzYzNhYmVlMTFmZWQ1MDhiOTk2MWFmNWQ=");
+
+        const requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow"
+        };
+
+        fetch("http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1", requestOptions)
+            .then((response) => response.text())
+            .then((result) => console.log(result))
+            .catch((error) => console.error(error));
+    }
+
+    const Player = (props) => {
+        return (
+            <div className={"flex justify-between px-10"}>
+                <h1 className={"text-center py-1"}>{props.position}</h1>
+                <h1 className={"text-center py-1"}>{props.name}</h1>
+                <h1 className={"text-center py-1"}>{props.avg}</h1>
+            </div>
+        )
+    }
+
+
+    const stats = [
+        {
+            image: "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Toronto_Blue_Jays_logo.svg/1200px-Toronto_Blue_Jays_logo.svg.png",
+            team: "BLUE JAYS",
+            i1: "0",
+            i2: "0",
+            i3: "0",
+            i4: "0",
+            i5: "0",
+            i6: "0",
+            i7: "0",
+            i8: "0",
+            i9: "0",
+            runs: "0",
+            hits: "0",
+            errors: "0",
+            balls: "0",
+            strikes: "0",
+            outs: "0"
+        },
+        {
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/New_York_Yankees_Primary_Logo.svg/1200px-New_York_Yankees_Primary_Logo.svg.png",
+            team: "Yankees",
+            i1: "0",
+            i2: "0",
+            i3: "0",
+            i4: "0",
+            i5: "0",
+            i6: "0",
+            i7: "0",
+            i8: "0",
+            i9: "0",
+            runs: "0",
+            hits: "0",
+            errors: "0",
+            balls: "0",
+            strikes: "0",
+            outs: "0"
+        }
+
+    ]
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      //main container for screen
+   <div className={"flex flex-col h-screen"}>
+     {/* Top Section - Picture and players */}
+     <div className={"flex"}>
+     {/* Width 3/4 - Player image */}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+         <div className={"w-3/4"}>
+              <img src={"https://pbs.twimg.com/media/FUYOL0EWAAEAXGE.jpg:large"} />
+         </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+         {/* Width 1/4 - Roster and stats*/}
+         <div className={"w-1/4 flex flex-col"}>
+             <div className={"border-2 border-white"}>
+                 <h1 className={"text-center py-1"}>TORONTO BLUE JAYS</h1>
+             </div>
+             <div className={"border-2 border-t-0 border-white"} style={{
+                 // background: "url('https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/New_York_Yankees_Primary_Logo.svg/1200px-New_York_Yankees_Primary_Logo.svg.png')",
+                 //    backgroundSize: "cover",
+                 //    backgroundPosition: "center",
+                 //    backgroundRepeat: "no-repeat",
+                 // backgroundColor: "rgba(0,0,0,0.5)",
+             }
+             }>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+             </div>
+             <div className={"border-2 border-t-0 border-white"}>
+                 <Player position={"P"} name={"REYES"} avg={".20"}/>
+             </div>
+             <div className={"border-2 border-white border-t-0"}>
+                 <h1 className={"text-center py-1"}>NEW YORK YANKEES</h1>
+             </div>
+             <div className={"border-2 border-t-0 border-white"}>
+                 <Player position={"LF"} name={"GARDNER"} avg={".29"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+                 <Player position={"SS"} name={"REYES"} avg={".20"}/>
+             </div>
+             <div className={"border-2 border-t-0 border-white"}>
+                 <Player position={"P"} name={"REYES"} avg={".20"}/>
+             </div>
+         </div>
+     </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+       {/* Bottom Section - Team Scores */}
+       <div>
+           <div className={"flex"}>
+               <Table className={" w-full"}>
+                   <TableHeader className={"border-y-2"}>
+                       <TableRow>
+                           <TableHead>YANKEE STADIUM</TableHead>
+                           <TableHead>1</TableHead>
+                           <TableHead>2</TableHead>
+                           <TableHead>3</TableHead>
+                           <TableHead>4</TableHead>
+                           <TableHead>5</TableHead>
+                           <TableHead>6</TableHead>
+                           <TableHead>7</TableHead>
+                           <TableHead>8</TableHead>
+                           <TableHead>9</TableHead>
+                           <TableHead>R</TableHead>
+                           <TableHead>H</TableHead>
+                           <TableHead>E</TableHead>
+                       </TableRow>
+                   </TableHeader>
+                   <TableBody>
+                       {stats.map((s) => (
+                           <TableRow key={s.team}>
+                               <TableCell>
+                                   <div className={"flex space-x-4 items-center uppercase text-3xl"}>
+                                       <img alt={s.team} className={"object-fit"} width={50} height={20} src={s.image} />
+                                       <h1>{s.team}</h1>
+                                   </div>
+                               </TableCell>
+                               <TableCell className={"text-3xl"}>{s.i1}</TableCell>
+                               <TableCell className={"text-3xl"}>{s.i2}</TableCell>
+                               <TableCell className={"text-3xl"}>{s.i3}</TableCell>
+                               <TableCell className={"text-3xl"}>{s.i4}</TableCell>
+                               <TableCell className={"text-3xl"}>{s.i5}</TableCell>
+                               <TableCell className={"text-3xl"}>{s.i6}</TableCell>
+                               <TableCell className={"text-3xl"}>{s.i7}</TableCell>
+                               <TableCell className={"text-3xl"}>{s.i8}</TableCell>
+                               <TableCell className={"text-3xl"}>{s.i9}</TableCell>
+                               <TableCell className={"text-3xl"}>{s.runs}</TableCell>
+                               <TableCell className={"text-3xl"}>{s.hits}</TableCell>
+                               <TableCell className={"text-3xl"}>{s.errors}</TableCell>
+                           </TableRow>
+                       ))}
+                   </TableBody>
+               </Table>
+               <div className={"w-1/3 border-y-white"}>
+                   <Table className={"w-full"}>
+                       <TableHeader className={"border-y-2"}>
+                           <TableRow>
+                               <TableHead className={"text-right"}>BALLS</TableHead>
+                                 <TableHead className={"text-right"}>STRIKES</TableHead>
+                                    <TableHead className={"text-right"}>OUTS</TableHead>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+                           </TableRow>
+                       </TableHeader>
+                       <TableBody>
+                               <TableRow>
+                                      <TableCell className={"text-7xl text-right"}>{0}</TableCell>
+                                        <TableCell className={"text-7xl text-right"}>{1}</TableCell>
+                                        <TableCell className={"text-7xl text-right"}>{1}</TableCell>
+
+                               </TableRow>
+                       </TableBody>
+                   </Table>
+               </div>
+           </div>
+       </div>
+   </div>
   )
 }
